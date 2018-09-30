1257a8809cb6c17f35b0cf3afedceaa4ff4f5c9c
Lpieri <42479462+lpiere@users.noreply.github.com>
2018-09-30 07:35:15 -0300

    Add files via upload

---
+++ arduinos/NANO.ino
@@ -0,0 +1,61 @@
+//Programa : RFID - Controle de Acesso leitor RFID
+//Autor : FILIPEFLOP
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
+
+void setup() 
+{
+  Serial.begin(9600); // Inicia a serial
+  SPI.begin();    // Inicia  SPI bus
+  mfrc522.PCD_Init();
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  digitalWrite(2,0);
+  digitalWrite(3,0);
+  digitalWrite(4,255);  
+}
+
+void loop() 
+{  
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Mostra UID na serial
+  String conteudo="";
+  byte letra;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+    conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : " "));
+    conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.print(conteudo.substring(1));
+  Serial.println("");
+  delay(500);
+  if(Serial.available()>0){
+    digitalWrite(2,0);
+    digitalWrite(3,255);
+    digitalWrite(4,0);
+  }else{
+    digitalWrite(2,255);
+    digitalWrite(3,0);
+    digitalWrite(4,0);
+  } 
+  delay(1000);
+  digitalWrite(2,0);
+  digitalWrite(3,0);
+  digitalWrite(4,255);
+} 
+
---
+++ arduinos/nodemcu.ino
@@ -0,0 +1,112 @@
+#include <ESP8266WiFi.h> 
+#include <PubSubClient.h>
+
+const char* SSID = "luan";                // SSID / nome da rede WiFi que deseja se conectar
+const char* PASSWORD = "12345678";  // Senha da rede WiFi que deseja se conectar
+WiFiClient wifiClient;  
+
+const char* BROKER_MQTT = "iot.eclipse.org"; //URL do broker MQTT que se deseja utilizar
+int BROKER_PORT = 1883;                      // Porta do Broker MQTT
+
+#define ID_MQTT "bco1"
+#define TOPICO "a"
+PubSubClient MQTT(wifiClient);        // Instancia o Cliente MQTT passando o objeto espClient
+
+void mantemConexoes();  //Garante que as conexoes com WiFi e MQTT Broker se mantenham ativas
+void conectaWiFi();     //Faz conexão com WiFi
+void conectaMQTT();     //Faz conexão com Broker MQTT
+
+
+void setup() {
+  pinMode(D0,OUTPUT);
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);  
+  Serial.begin(115200);
+  conectaWiFi();
+  MQTT.setServer(BROKER_MQTT, BROKER_PORT);  
+  MQTT.setCallback(recebePacote); 
+}
+
+void loop() {
+  Serial.println("não há ninguém");
+  digitalWrite(D0,0);
+  digitalWrite(D1,0);
+  digitalWrite(D2,255);  
+  mantemConexoes();
+  if (!MQTT.connected()) 
+       conectaMQTT(); 
+  MQTT.loop();
+  
+}
+
+void mantemConexoes() {
+  if (WiFi.status() == WL_CONNECTED) {
+     return;
+  }
+    
+    conectaWiFi(); //se não há conexão com o WiFI, a conexão é refeita
+}
+
+void conectaWiFi() {
+
+
+        
+  Serial.print("Conectando-se na rede: ");
+  Serial.print(SSID);
+  Serial.println("  Aguarde!");
+
+  WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI  
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(100);
+      Serial.print(".");
+  }
+  
+  Serial.println();
+  Serial.print("Conectado com sucesso, na rede: ");
+  Serial.print(SSID);  
+  Serial.print("  IP obtido: ");
+  Serial.println(WiFi.localIP()); 
+}
+
+void conectaMQTT() { 
+    while (!MQTT.connected()) {
+        Serial.print("Conectando ao Broker MQTT: ");
+        Serial.println(BROKER_MQTT);
+        if (MQTT.connect(ID_MQTT)) {
+            Serial.println("Conectado ao Broker com sucesso!");
+            MQTT.subscribe(TOPICO);
+        } 
+        else {
+            Serial.println("Noo foi possivel se conectar ao broker.");
+            Serial.println("Nova tentatica de conexao em 10s");
+            delay(10000);
+        }
+    }
+}
+
+void recebePacote(char* topic, byte* payload, unsigned int length) 
+{
+      String msg;
+    //obtem a string do payload recebido
+    for(int i = 0; i < length; i++) 
+    {
+       char c = (char)payload[i];
+       msg += c;
+    }
+    if (msg == "0") {
+       Serial.println("não está apropriado");
+       digitalWrite(D0,255);
+       digitalWrite(D1,0);
+       digitalWrite(D2,0);       
+    }else{
+      if (msg == "1") {
+         Serial.println("está apropriado");
+         digitalWrite(D0,0);
+         digitalWrite(D1,255);
+         digitalWrite(D2,0);       
+      }
+      delay(5000);
+   }
+    
+}
+
